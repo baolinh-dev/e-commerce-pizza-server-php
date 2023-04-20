9d7e6d603c96118a2b320748db617b91b417b2b5
baolinh <107604287+baolinh-dev@users.noreply.github.com>
2023-04-20 10:32:07 +0700

    handle avatar user

--- dangki.php
+++ dangki.php
@@ -11,39 +11,67 @@ $email = $_POST["email"];
 $phone = $_POST["phone"]; 
 $fullname = $_POST["fullname"];
 $role = $_POST["role"]; // lấy giá trị của trường "role" từ phía client
+$host = gethostbyname(gethostname());
 
-// Hash mật khẩu với mã SHA-256
-$hashed_password = hash('sha256', $password);
+// Xử lý tệp tin hình ảnh avatar
+$file = $_FILES["avatar"];
+$target_dir = "D:/workspace/web/personal-project/e-commerce-pizza/e-commerce-pizza-web/public/imgs/";
+$target_file = $target_dir . basename($file["name"]);
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-$sql = "SELECT * FROM user WHERE username='$username'";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    echo "Tên đăng nhập đã được sử dụng, vui lòng chọn tên đăng nhập khác";
+// Kiểm tra file hình ảnh có phù hợp không
+if($file["size"] > 5000000) {
+    echo "Kích thước tệp tin quá lớn. Vui lòng chọn tệp tin có kích thước nhỏ hơn hoặc bằng 5MB.";
     exit();
-} 
+}
+if($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png" && $imageFileType != "gif") {
+    echo "Chỉ cho phép tải lên các tệp tin JPG, JPEG, PNG hoặc GIF.";
+    exit();
+}
 
-$sql = "INSERT INTO user (username, password, fullname, email, phone, role) VALUES ('$username', '$hashed_password', '$fullname', '$email', '$phone', '$role')";
+// Upload tệp tin hình ảnh vào thư mục /public/imgs
+if (move_uploaded_file($file["tmp_name"], $target_file)) {
+    $avatar = "http://" . $host . ":3000/imgs/" . basename($file["name"]);
 
-if (mysqli_query($conn, $sql)) {
-    $new_id = mysqli_insert_id($conn);
-    $new_user = [
-        'id' => $new_id,
-        'username' => $username,
-        'fullname' => $fullname,
-        'email' => $email,
-        'phone' => $phone,
-        'role' => $role
-    ];
-    $arr = [ 
-        'success' => true, 
-        'message' => "Thành công",
-        'result' => $new_user
-    ];
+    // Hash mật khẩu với mã SHA-256
+    $hashed_password = hash('sha256', $password);
+
+    $sql = "SELECT * FROM user WHERE username='$username'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "Tên đăng nhập đã được sử dụng, vui lòng chọn tên đăng nhập khác";
+        exit();
+    } 
+
+    $sql = "INSERT INTO user (id, username, avatar, password, fullname, email, phone, role) VALUES ('$new_id', '$username', '$avatar', '$hashed_password', '$fullname', '$email', '$phone', '$role')";
+
+    if (mysqli_query($conn, $sql)) {
+        $new_id = mysqli_insert_id($conn);
+        $new_user = [
+            'id' => $new_id,
+            'username' => $username,
+            'fullname' => $fullname,
+            'email' => $email,
+            'phone' => $phone,
+            'role' => $role,
+            'avatar' => $avatar
+        ];
+        $arr = [ 
+            'success' => true, 
+            'message' => "Thành công",
+            'result' => $new_user
+        ];
+    } else {
+        $arr = [ 
+            'success' => false, 
+            'message' => "Không thành công"
+        ];
+    }
 } else {
     $arr = [ 
         'success' => false, 
-        'message' => "Không thành công"
+        'message' => "Không thể tải lên tệp tin hình ảnh"
     ];
 }
 
--- updateuser.php
+++ updateuser.php
@@ -1,4 +1,5 @@
-<?php 
+<?php  
+header('Access-Control-Allow-Origin: *');
 include "connect.php";
 
 if (!$conn) {
@@ -13,10 +14,71 @@ $phone = $_POST['phone'];
 $fullname = $_POST["fullname"];
 $role = $_POST["role"]; // lấy giá trị của trường "role" từ phía client
 
+// Lưu file ảnh đại diện vào thư mục "imgs/" trên server
+$target_dir = "D:/workspace/web/personal-project/e-commerce-pizza/e-commerce-pizza-web/public/imgs/";
+$target_file = $target_dir . basename($_FILES["avatar"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Kiểm tra tính hợp lệ của file ảnh
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["avatar"]["tmp_name"]);
+    if($check !== false) {
+        $uploadOk = 1;
+    } else {
+        $uploadOk = 0;
+        $arr = [ 
+            'success' => false, 
+            'message' => "File tải lên không phải là ảnh"
+        ];
+        print_r(json_encode($arr));
+        exit();
+    }
+}
+// Kiểm tra kích thước của file ảnh
+if ($_FILES["avatar"]["size"] > 500000) {
+    $uploadOk = 0;
+    $arr = [ 
+        'success' => false, 
+        'message' => "Kích thước ảnh đại diện phải nhỏ hơn 500KB"
+    ];
+    print_r(json_encode($arr));
+    exit();
+}
+// Cho phép tải lên các định dạng ảnh sau: JPG, JPEG, PNG, GIF
+if($imageFileType != "jpg" && $imageFileType != "jpeg" && $imageFileType != "png" && $imageFileType != "gif" ) {
+    $uploadOk = 0;
+    $arr = [ 
+        'success' => false, 
+        'message' => "Chỉ cho phép tải lên các file ảnh định dạng: JPG, JPEG, PNG, GIF"
+    ];
+    print_r(json_encode($arr));
+    exit();
+}
+if ($uploadOk == 0) {
+    $arr = [ 
+        'success' => false, 
+        'message' => "Tải lên ảnh đại diện thất bại"
+    ];
+    print_r(json_encode($arr));
+    exit();
+} else {
+    // Nếu các kiểm tra đều đã được thông qua, lưu ảnh đại diện vào thư mục "imgs/" trên server
+    if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $target_file)) {
+        $avatar = "http://" . $_SERVER['HTTP_HOST'] . ":3000/imgs/" . basename($_FILES["avatar"]["name"]);
+    } else {
+        $arr = [ 
+            'success' => false, 
+            'message' => "Tải lên ảnh đại diện thất bại"
+        ];
+        print_r(json_encode($arr));
+        exit();
+    }
+}
+
 // Hash mật khẩu với mã SHA-256
 $hashed_password = hash('sha256', $password);
 
-$sql = "UPDATE user SET username='$username', password='$hashed_password', email='$email', phone='$phone', fullname='$fullname', role='$role' WHERE id='$id'";
+$sql = "UPDATE user SET username='$username', password='$hashed_password', email='$email', phone='$phone', fullname='$fullname', role='$role', avatar='$avatar' WHERE id='$id'";
 
 if (mysqli_query($conn, $sql)) {
     $new_user = [
@@ -25,7 +87,8 @@ if (mysqli_query($conn, $sql)) {
         'fullname' => $fullname,
         'email' => $email,
         'phone' => $phone,
-        'role' => $role
+        'role' => $role,
+        'avatar' => $avatar
     ];
     $arr = [ 
         'success' => true, 
